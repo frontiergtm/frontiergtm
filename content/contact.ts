e44ffc87b7752a52448085c510ba9a1904182eb5
Ryan Pollock <ryan.pollock@gmail.com>
2026-06-15 11:15:42 -0700

    back to svg

--- content/contact.ts
+++ content/contact.ts
@@ -1,11 +1,1 @@
-const consultationSubject = "Requesting a Free Consultation";
-const consultationBody = `Hi Ryan,
-
-I would like to speak with you regarding [fill in the blank].
-
-Regards,
-Your Name`;
-
-export const consultationMailto = `mailto:ryan@frontiergtm.ai?subject=${encodeURIComponent(
-  consultationSubject,
-)}&body=${encodeURIComponent(consultationBody)}`;
+export const consultationMailto = "mailto:ryan@frontiergtm.ai";
